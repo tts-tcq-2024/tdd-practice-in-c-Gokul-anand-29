e45373adcdd40db9620f9804af0cdd1d1a54d698
Gokul-anand-29 <gokulanand200@gmail.com>
2024-11-03 19:31:28 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -50,13 +50,6 @@ TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     ASSERT_EQ(result, expectedresult);
 }
 
-TEST(StringCalculatorAddTests, ExpectExeptionForNegativeNumbers) {
-    int expectedresult = 0;
-    const char*  input = "-1";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
 TEST(StringCalculatorAddTests, ExpectSumOfNumbersEqualto1000) {
     int expectedresult = 1999;
     const char*  input = "999,1000";
